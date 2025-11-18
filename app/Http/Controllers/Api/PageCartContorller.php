daf31cddcefea641025c844de53aec33e405c0ba
hexaviate <hexaviate@gmail.com>
2025-11-18 09:19:05 +0700

    push coba

--- app/Http/Controllers/Api/PageCartContorller.php
+++ app/Http/Controllers/Api/PageCartContorller.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class PageCartContorller
 {
 
-    public function infoCart()
+    public function infoCart(Request $request)
     {
         $user = auth('sanctum')->user();
 
